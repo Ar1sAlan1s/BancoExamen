9f25cf1c718480012c602083e2e456d765273a63
dorian <141415229+DorianTR3@users.noreply.github.com>
2024-05-15 02:13:03 -0700

    actualizacion

--- src/Tarjetas/Debito.java
+++ src/Tarjetas/Debito.java
@@ -9,50 +9,36 @@ public class Debito extends Tarjeta {
     }
 
     public void agregarDinero() {
-        while(band) {
+        while (true) {
             try {
                 System.out.println("Ingresa el dinero a agregar:");
-                double dineroAgregado = scan.nextDouble();
+                double dineroAgregado = this.scan.nextDouble();
                 this.setSaldo(this.getSaldo() + dineroAgregado);
-                this.band = false;
-            } catch (InputMismatchException e) {
-                System.out.println("Ingresa un numero de dinero");
+                break;
+            } catch (InputMismatchException var3) {
+                System.out.println("Ingresa un número de dinero.");
                 this.scan.nextLine();
             }
         }
-
     }
 
     public void retirarDinero() {
-        while(band) {
+        while(this.band) {
             try {
                 System.out.println("Ingresa el dinero a retirar:");
-                double dineroRetirado = scan.nextDouble();
-                if (dineroRetirado <= getSaldo()) {
-                    this.setSaldo(getSaldo() - dineroRetirado);
+                double dineroRetirado = this.scan.nextDouble();
+                if (dineroRetirado <= this.getSaldo()) {
+                    this.setSaldo(this.getSaldo() - dineroRetirado);
                     this.band = false;
                 } else {
                     System.out.println("No tienes el suficiente dinero");
                 }
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException var3) {
                 System.out.println("Ingresa un numero de dinero");
                 this.scan.nextLine();
             }
         }
 
-    }
-
-    public void mostrarTarjeta(){
-        System.out.println("Tipo: "+ TiposCredito.debito);
-        System.out.println("Numero de tarjeta: "+ super.numeroDeTarjeta);
-        System.out.println("Fecha creacion: "+ super.fechaDeCreacion);
-        System.out.println("Fecha vencimiento: "+ super.fechaDeCreacion.toString());
-        System.out.println("Saldo: "+super.saldo );
-        System.out.println("Clave interbancaria: "+ super.getClabe());
-        System.out.println("CVV: "+ super.CVV);
-        System.out.println("Fecha ultimo movimiento: "+ super.fechaDeUltimoMovimiento.toString());
-        System.out.println("Fecha ultimo movimiento: "+ super.getHoraDeUltimoMovimiento().toString());
-        System.out.println();
     }
 
     public void mostrarSaldo() {
